cdda4d1d2e13c09ef5b311676b733fafd50dbfb8
AbdulazizAlkhars <alkharswork@gmail.com>
2022-07-04 19:43:35 +0300

    Better way of solving it

--- calculator.py
+++ calculator.py
@@ -1,19 +1,3 @@
-
-# def main():
-# write your code here
-valid = False
-while not valid:
-    try:
-        num1 = int(input("Enter the first number:"))
-        num2 = int(input("Enter the second number:"))
-        valid = True
-    except ValueError:
-        print("Number is not valid")
-
-
-operation1 = input("Choose the operation (+, -, /, *):")
-
-
 def calculator(number1, number2, operationType):
     if operationType == "+":
         answer = int(number1)+int(number2)
@@ -31,12 +15,25 @@ def calculator(number1, number2, operationType):
         return None
 
 
-answer = calculator(num1, num2, operation1)
-if answer == None:
-    print("Operation not valid")
-else:
-    print("The answer is", str(answer))
+def main():
+    # write your code here
+    valid = False
+    while not valid:
+        try:
+            num1 = int(input("Enter the first number:"))
+            num2 = int(input("Enter the second number:"))
+            valid = True
+        except ValueError:
+            print("Number is not valid")
+
+    operation1 = input("Choose the operation (+, -, /, *):")
+
+    answer = calculator(num1, num2, operation1)
+    if answer == None:
+        print("Operation not valid")
+    else:
+        print("The answer is", str(answer))
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
